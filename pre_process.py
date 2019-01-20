0ed97898f28d29dcae8d095eb6fbee93132f03de
unknown <foamliu@yeah.net>
2019-01-20 13:46:08 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -36,28 +36,34 @@ if __name__ == "__main__":
     if not os.path.isdir('data/CASIA-WebFace'):
         extract('data/CASIA-WebFace.zip')
 
-    samples = []
     subjects = [d for d in os.listdir('data/CASIA-WebFace') if os.path.isdir(os.path.join('data/CASIA-WebFace', d))]
     assert (len(subjects) == 10575), "Number of subjects is: {}!".format(len(subjects))
 
-    for i in tqdm(range(len(subjects))):
+    file_names = []
+
+    for i in range(len(subjects)):
         sub = subjects[i]
         folder = os.path.join('data/CASIA-WebFace', sub)
-        # print(folder)
         files = [f for f in os.listdir(folder) if
                  os.path.isfile(os.path.join(folder, f)) and f.lower().endswith('.jpg')]
-        # print(files)
         for file in files:
             filename = os.path.join(folder, file)
-            # print(filename)
-            is_valid, landmarks = get_face_attributes(filename)
-            if is_valid:
-                samples.append(
-                    {'class_id': i, 'subject': sub, 'full_path': filename, 'landmarks': landmarks})
+            file_names.append({'filename': filename, 'class_id': i, 'subject': sub})
 
-    f = open(pickle_file, 'wb')
-    save = {
-        'samples': samples
-    }
-    pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
-    f.close()
+    assert (len(file_names) == 494414), "Number of files is: {}!".format(len(file_names))
+
+    samples = []
+    for item in tqdm(file_names):
+        filename = item['filename']
+        class_id = item['class_id']
+        sub = item['subject']
+        is_valid, landmarks = get_face_attributes(filename)
+        if is_valid:
+            samples.append(
+                {'class_id': class_id, 'subject': sub, 'full_path': filename, 'landmarks': landmarks})
+
+    with open(pickle_file, 'wb') as file:
+        save = {
+            'samples': samples
+        }
+        pickle.dump(save, file, pickle.HIGHEST_PROTOCOL)
