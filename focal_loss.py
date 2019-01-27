46bd0725d8362caba539cc53448ba9d6b4506bb8
unknown <foamliu@yeah.net>
2019-01-27 19:39:54 +0800

    update

--- focal_loss.py
+++ focal_loss.py
@@ -1,43 +1,41 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-
-
-# class FocalLoss(nn.Module):
-#
-#     def __init__(self, gamma=0, eps=1e-7):
-#         super(FocalLoss, self).__init__()
-#         self.gamma = gamma
-#         self.eps = eps
-#         self.ce = torch.nn.CrossEntropyLoss()
-#
-#     def forward(self, input, target):
-#         logp = self.ce(input, target)
-#         p = torch.exp(-logp)
-#         loss = (1 - p) ** self.gamma * logp
-#         return loss.mean()
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, size_average=True):
+
+    def __init__(self, gamma=0, eps=1e-7):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.size_average = size_average
+        self.eps = eps
+        self.ce = torch.nn.CrossEntropyLoss()
 
     def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1)
+        logp = self.ce(input, target)
+        p = torch.exp(-logp)
+        loss = (1 - p) ** self.gamma * logp
+        return loss.mean()
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
-
-        loss = -1 * (1 - pt) ** self.gamma * logpt
-        if self.size_average:
-            return loss.mean()
-        else:
-            return loss.sum()
+# class FocalLoss(nn.Module):
+#     def __init__(self, gamma=0, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         self.gamma = gamma
+#         self.size_average = size_average
+#
+#     def forward(self, input, target):
+#         if input.dim() > 2:
+#             input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+#             input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
+#             input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
+#         target = target.view(-1, 1)
+#
+#         logpt = F.log_softmax(input)
+#         logpt = logpt.gather(1, target)
+#         logpt = logpt.view(-1)
+#         pt = Variable(logpt.data.exp())
+#
+#         loss = -1 * (1 - pt) ** self.gamma * logpt
+#         if self.size_average:
+#             return loss.mean()
+#         else:
+#             return loss.sum()
